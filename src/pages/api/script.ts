446ff10ed8a4a0b8ee31d12f6b8e2e7a6d060dd9
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-02-29 23:21:07 +0530

    fix: script path2

--- src/pages/api/script.ts
+++ src/pages/api/script.ts
@@ -26,9 +26,10 @@ export default function handler(
     // exec command to create repo using github
   }
 
-  exec(`bash ./backend/run.sh ${username} ${repo} ${no_of_days} ${past}`, (error, stdout, stderr) => {
+  exec(`bash /backend/run.sh ${username} ${repo} ${no_of_days} ${past}`, (error, stdout, stderr) => {
     if (error) {
       console.error("Got an Error : ", stderr);
+      res.status(501).json({ msg: stderr });
     }
     console.log({ stdout });
     res.status(200).json({ msg: "Successfull" });
