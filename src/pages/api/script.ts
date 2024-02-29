45f0cbcaa8c370fdfa4ce43613c7df796bea8703
Sonal144 <143083452+Sonal144@users.noreply.github.com>
2024-02-29 23:05:19 +0530

    chore: add code for backend

---
+++ src/pages/api/script.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+// import "../../backend/run.sh";
+import { exec } from "child_process";
+import path from "path";
+
+type Data = {
+  msg: string;
+};
+
+// Usage: $0 <username> <repo> <no._of_days> <past?:optoinal>
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { username, repo, no_of_days, past } = req.body;
+
+  // validate if all the params are present
+  if (!username || !no_of_days || !past) {
+    res.status(401).json({ msg: "provide all fields" });
+  }
+
+  // handling repo not provided
+  if (repo === null || repo === undefined) {
+    // exec command to create repo using github
+  }
+
+  exec(`bash ./public/backend/run.sh ${username} ${repo} ${no_of_days} ${past}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error("Got an Error : ", stderr);
+    }
+    console.log({ stdout });
+    res.status(200).json({ msg: "Successfull" });
+  });
+}
